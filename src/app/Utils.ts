8cae9106d285565775cc68e5b596ef24dd39c937
elarsaks <elarsaks@gmail.com>
2021-09-13 19:24:43 +0300

    Do some basic testing.

--- src/app/Utils.ts
+++ src/app/Utils.ts
@@ -1,5 +1,13 @@
+import { UrlWithParsedQuery, parse } from "url";
+
 export class Utils {
+
+	public static parseUrl(url: string): UrlWithParsedQuery {
+		return parse(url, true);
+	}
+	
+
 	public static toUpperCase(arg:string) {
-		return '';
+		return arg.toUpperCase();
 	}
 }
--- src/test/Utils.test.ts
+++ src/test/Utils.test.ts
@@ -1,8 +1,27 @@
 import {Utils} from '../app/Utils'
 
 describe('Urils test suite', () => {
-	test('first test', () => {
-		const abc = Utils.toUpperCase('abc')
-		console.log('Tests work!')
+	test('First test', () => {
+		const result = Utils.toUpperCase('abc');
+		expect(result).toBe('ABC');
+	});
+
+	test('Parse simple URl', () => {
+		const parsedUrl = Utils.parseUrl('http://localhost:8080/login');
+		expect(parsedUrl.href).toBe('http://localhost:8080/login');
+		expect(parsedUrl.port).toBe('8080');
+		expect(parsedUrl.protocol).toBe('http:');
+		expect(parsedUrl.query).toEqual({});
+	});
+
+	test('Parse URL with query', () => {
+		const parsedUrl = Utils.parseUrl('http://localhost:8080/login?user=user&password=pass');
+		const expectedQuery = {
+			user: 'user',
+			password: 'pass'
+		}
+		expect(parsedUrl.query).toEqual(expectedQuery);
+		expect(expectedQuery).toBe(expectedQuery);
 	})
+
 });
